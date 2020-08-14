517dc146cdce1f9507b2db06defc9533c7326af5
owolabioromidayo <owolabi.oromidayo@stu.cu.edu.ng>
2020-08-14 19:07:28 +0100

    Complete Model + Loading functionality

---
+++ utils.py
@@ -0,0 +1,14 @@
+from keras.models import model_from_json
+
+def load_model():
+    json_file = open('model.json', 'r')
+    loaded_model_json = json_file.read()
+    json_file.close()
+
+    loaded_model = model_from_json(loaded_model_json)
+    loaded_model.load_weights("model.h5")
+    print("Loaded model from disk")
+
+    # loaded_model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+    # score = loaded_model.evaluate(X, Y, verbose=0)
+    # print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1]*100))
